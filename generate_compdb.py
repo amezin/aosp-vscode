747de98ffbb5805eefc48a0b5c6ea6b9e561654a
Aleksandr Mezin <mezin.alexander@gmail.com>
2018-04-05 19:50:18 +0600

    A bit less clunky subcommand handling

--- generate_compdb.py
+++ generate_compdb.py
@@ -11,7 +11,12 @@ RULE_PATTERN = re.compile(r'^\s*rule\s+(\S+)$')
 COMMAND_PATTERN = re.compile(r'^\s*command\s*=\s*(.+)$')
 BUILD_PATTERN = re.compile(r'^\s*build\s+.*:\s*(?P<rule>\S+)\s+(?P<file>\S+)')
 CAT_PATTERN = re.compile(r'\\\$\$\(\s*cat\s+([^\)]+)\)')
-SUBCOMMAND_PATTERN = re.compile(r'\([^\)]*\)')
+SUBCOMMAND_PATTERN = re.compile(r'\(([^\)]*)\)')
+
+rules = {}
+compdb = []
+directory = os.path.abspath(os.getcwd())
+
 
 def cat_expand(match):
     try:
@@ -21,13 +26,33 @@ def cat_expand(match):
         print(ex, file=sys.stderr)
 
 
+def parse_command(command):
+    while command:
+        first_space = command.find(' ', 1)
+        if (first_space == -1):
+            first_space = len(command)
+
+        if command[0:first_space].endswith('/clang') or command[0:first_space].endswith('/clang++'):
+            break
+
+        command = command[first_space:]
+
+    command = command.strip()
+
+    if not command:
+        return False
+
+    compdb.append({
+        'directory': directory,
+        'command': command,
+        'file': build_match.group('file')
+    })
+    return True
+
+
 parser = argparse.ArgumentParser(description='Generate compile_commands.json for AOSP')
 parser.add_argument('ninja_file')
 args = parser.parse_args()
-
-rules = {}
-compdb = []
-directory = os.path.abspath(os.getcwd())
 
 with open(args.ninja_file) as ninja_file:
     for line in ninja_file:
@@ -46,30 +71,13 @@ with open(args.ninja_file) as ninja_file:
             command = rules.get(build_match.group('rule'))
             if command:
                 command = CAT_PATTERN.sub(cat_expand, command)
-                subcommands = SUBCOMMAND_PATTERN.findall(command)
-                if subcommands:
-                    command = subcommands[0][1:-1]
-
-                while command:
-                    first_space = command.find(' ', 1)
-                    if (first_space == -1):
-                        first_space = len(command)
-
-                    if command[0:first_space].endswith('/clang') or command[0:first_space].endswith('/clang++'):
+                has_subcommands = False
+                for subcommand in SUBCOMMAND_PATTERN.finditer(command):
+                    has_subcommands = True
+                    if parse_command(subcommand.group(1)):
                         break
-
-                    command = command[first_space:]
-
-                command = command.strip()
-
-                if not command:
-                    continue
-
-                compdb.append({
-                    'directory': directory,
-                    'command': command,
-                    'file': build_match.group('file')
-                })
+                if not has_subcommands:
+                    parse_command(command)
 
 with open('out/compile_commands.json', 'w') as compdb_file:
     json.dump(compdb, compdb_file, indent=1)
